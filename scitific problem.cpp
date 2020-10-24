8dc69826c27637c6c482971652b57451203bc35f
raghavraghs <44517942+raghavraghs@users.noreply.github.com>
2020-10-24 11:17:48 +0530

    Update and rename scitific problem.c to scitific problem.cpp

---
+++ scitific problem.cpp
@@ -0,0 +1,12 @@
+By ivvraghavendra199, contest: acmsguru, problem: (403) Scientific Problem, Accepted, #
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() {
+    ll n;
+    cin >> n
+    ll k = (2*n)+1;
+    cout << k;
+    return 0;
+}
